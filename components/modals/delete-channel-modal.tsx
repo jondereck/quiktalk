f4d572de91edd7357494b7f75dd234b30daaffdb
jondereck <jonderecknifas@gmail.com>
2023-09-03 18:03:23 +0800

    done-server-sidebar

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import qs from "query-string";
 import axios from "axios";
 import { useState } from "react";
 
@@ -13,29 +14,46 @@ import {
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
 import { DialogDescription } from "@radix-ui/react-dialog";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { toast } from "../ui/use-toast";
 
-export const DeleteServerModal = () => {
+export const DeleteChannelModal = () => {
   const router = useRouter();
-  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const {isOpen, onClose, type, data } = useModal();
 
 
-  const isModalOpen = isOpen && type === "deleteServer";
-  const { server } = data;
+  const isModalOpen = isOpen && type === "deleteChannel";
+  const { server, channel } = data;
 
   const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
     try {
       setIsLoading(true);
-      
-      await axios.delete(`/api/servers/${server?.id}`);
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id
+        }
+      })
+      await axios.delete(url);
+
+      toast({
+        title: "Success",
+        description: `${channel?.name} has been deleted.`
+      })
 
       onClose();
       router.refresh();
-      router.push("/");
+      router.push(`/servers/${server?.id}`);
       
     } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      })
       console.log(error);
     } finally {
       setIsLoading(false);
@@ -49,11 +67,11 @@ export const DeleteServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Delete Server
+            Delete Channel
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Are you sure you want to this? <br/> 
-            <span className="font-semibold text-[#E45826]">{server?.name}</span> will be permanently deleted.
+            <span className="font-semibold text-[#E45826]">{channel?.name}</span> will be permanently deleted.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
